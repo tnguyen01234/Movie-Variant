e3a3b39ec97cd51b7126cb0f8ba58dab08f44198
TommyZenbookPro <nguyentommy735@gmail.com>
2022-11-14 15:57:46 +1100

    Layout Index complete

---
+++ index.js
@@ -0,0 +1,4 @@
+// IP Request 
+// All Data Requests http://www.omdbapi.com/?apikey=4148fa0f&
+// All Poster Request http://img.omdbapi.com/?apikey=?
+// Data about movie. https://www.omdbapi.com/?i=tt7161870&apikey=4148fa0f
